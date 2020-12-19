e2c52ae05eb6980a9a0f8b631741fc27ab953a1e
beyanchan <75328415+beyanchan@users.noreply.github.com>
2020-12-19 15:54:26 +0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,13 +1,25 @@
 from tobrot.sample_config import Config
-#Fill your all data, read readme for reference
 
 class Config(Config):
-    TG_BOT_TOKEN= "1423837215:AAHD2S1EoTkPEtefGwNJwalD1rO-wtPv0JA"
-    APP_ID = 2533831
-    API_HASH = "cd35733b01c7da53a462e14f048291b4"
-    OWNER_ID = 1323460577
-    AUTH_CHANNEL = [-1001396933879]
-    DESTINATION_FOLDER = "TorrentLeech-Gdrive" #Name of your folder read readme(not id of the folder)
-    RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMCA97PSbVnmQGcDS_JhHXcOw0CySDGxjzZyWjmrgWHDf9A1n-Rmdg4sVnUDxV9Y5_5tAdbhfpW6_xbF1O9ALAeBj1xmKFQtMubDyGfYB_i1LltJA_e_zJawP-peIK4yzoYrZTvdfP02U2PHfqHiV6WmX5RCUPmXOqHlMG4","token_type":"Bearer","refresh_token":"1//0guQVZQz65RjnCgYIARAAGBASNwF-L9IrWy7b2vaAF3Qyh-UjCszb0xVSG83KYCi4X9taiSfr_9EhDO13oqWNTuoVGris4xENImo","expiry":"2020-12-03T13:41:27.382397538Z"}"""
-    #fill taking reference of this config, dont remove """ from both side of the RCLONE_CONFIG variable 
-    #FOR CUSTOM COMMANDS READ REAME AND FILL THEM...
+  TG_BOT_TOKEN = "1471375127:AAGNQRrO-IS7b3TBmELBWPwQDo_kQDizpOY"
+  APP_ID = 2105857
+  API_HASH = "c8aacc9417551dc98e2b2f61fbfb8387"
+  AUTH_CHANNEL = [-468168430, -1001408445549, -1001398883981, -1001275361660, -1001273851623]
+  INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/3:/Uploads"
+  YTDL_COMMAND = "ytdl@Leechz_Robot"
+  LEECH_COMMAND = "leech@Leechz_Robot"
+  CLEAR_THUMBNAIL = "clearthumbnail@Leechz_Robot"
+  SAVE_THUMBNAIL = "savethumbnail@Leechz_Robot"
+  OWNER_ID = 715779594
+  RCLONE_CONFIG = """type = drive
+scope = drive
+root_folder_id = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4
+token = {"access_token":"ya29A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}
+team_drive = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4"""
+  DESTINATION_FOLDER = ""
+  GLEECH_COMMAND = "gleech@Leechz_Robot"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@Leechz_Robot"
+  PYTDL_COMMAND_G = "pytdl@Leechz_Robot"
+  MAX_TG_SPLIT_FILE_SIZE = 2097152000
+  CANCEL_COMMAND_G = "cancel@Leechz_Robot"
+  STATUS_COMMAND = "status@Leechz_Robot"
